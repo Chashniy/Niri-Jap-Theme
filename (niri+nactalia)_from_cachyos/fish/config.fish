944924893e764ecd394bf64faf5837373ff930cb
Chashniy <grenka094@gmail.com>
2026-04-20 23:56:12 +0300

    niri/niri+noctalia

---
+++ (niri+nactalia)_from_cachyos/fish/config.fish
@@ -0,0 +1,11 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
+
+# быстрые команды для управления zapret
+alias zapret-config='$HOME/zapret-configs/install.sh'
+alias zapret-utils='$HOME/zapret-configs/utils-zapret.sh'
